02f97514cf2e67445856eba4d3b3db851f3c323a
Caleb Wolthers <w0473807@nscc.ca>
2024-02-27 10:53:56 -0400

    UI Challenge 2

--- Assets/Scripts/UI_Script.cs
+++ Assets/Scripts/UI_Script.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class UI_Script : MonoBehaviour
 {
@@ -14,48 +15,102 @@ public class UI_Script : MonoBehaviour
 
     public TMPro.TMP_Text textScore;
 
-    //public TextMeshProUGUI textScore;
 
+    public TMPro.TMP_Text textCoin;
+
+
+    public GameObject coin;
+
+
+    public Image imageRGB;
+
+
+    public Slider redSlider;
+    public Slider greenSlider;
+    public Slider blueSlider;
+    public Slider alphaSlider;
 
 
     private void Start()
     {
-        //textScore = GetComponent<TMP_Text>();
-
         score = 0;
 
         textScore.text = "Score: " + score;
+
+        textCoin.text = "Hide";
+
+
+        redSlider.maxValue = 1.0f;
+        greenSlider.maxValue = 1.0f;
+        blueSlider.maxValue = 1.0f;
+        alphaSlider.maxValue = 1.0f;
+
+        redSlider.minValue = 0.0f;
+        greenSlider.minValue = 0.0f;
+        blueSlider.minValue = 0.0f;
+        alphaSlider.minValue = 0.0f;
+
+
+        redSlider.value = 1.0f;
+        greenSlider.value = 1.0f;
+        blueSlider.value = 1.0f;
+        alphaSlider.value = 1.0f;
     }
 
-
+    private void Update()
+    {
+        Color newColor = new Color(redSlider.value, greenSlider.value, blueSlider.value, alphaSlider.value);
+        imageRGB.color = newColor;
+    }
 
     public void UpdateScore(int num)
     {
         if (num == 0)
         {
             score = 0;
-            textScore.text = "Score: " + score;
+
         }
         else 
         {
             score += num;
 
-            if (score >= 1000000000)
-            {
-                score = 999999999;
-            }
-            else if (score <= -100000000)
-            {
-                score = -99999999;
-            }
-
-            textScore.text = "Score: " + score;
-            
-            
+            score = Mathf.Clamp(score, 0, 999999999);
         }
-
+        textScore.text = "Score: " + score;
     }
 
+
+
+    public void RandomScore()
+    {
+        var rd = Random.Range(0, 999999999);
+
+        score = rd;
+
+        textScore.text = "Score: " + score;
+    }
+
+
+
+    public void HideCoin()
+    {
+        if (coin.activeInHierarchy)
+        {
+            coin.SetActive(false);
+            textCoin.text = "Show";
+        }
+        else
+        {
+            coin.SetActive(true);
+            textCoin.text = "Hide";
+        }
+        
+    }
+
+    public void ExitGame()
+    { 
+        Application.Quit();
+    }
 
 
 
