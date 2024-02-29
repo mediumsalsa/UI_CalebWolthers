bae930c61fe72996805ebaf7ec9a1f9ec9e1947e
Caleb Wolthers <w0473807@nscc.ca>
2024-02-29 11:34:35 -0400

    progress bar

--- Assets/Scripts/UI_Script.cs
+++ Assets/Scripts/UI_Script.cs
@@ -16,14 +16,13 @@ public class UI_Script : MonoBehaviour
     public TMPro.TMP_Text textScore;
 
 
-    public TMPro.TMP_Text textCoin;
+    public TMPro.TMP_Text textPNG;
 
 
-    public GameObject coin;
+    public GameObject imagePNG;
 
 
     public Image imageRGB;
-
 
     public Slider redSlider;
     public Slider greenSlider;
@@ -31,13 +30,25 @@ public class UI_Script : MonoBehaviour
     public Slider alphaSlider;
 
 
+    public Button buttonCoin;
+    public Slider progressBar;
+
+
     private void Start()
     {
+        //
+        progressBar.minValue = 0.0f;
+        progressBar.maxValue = 10.0f;
+        progressBar.value = 0.0f;
+
+
         score = 0;
 
         textScore.text = "Score: " + score;
 
-        textCoin.text = "Hide";
+        textPNG.text = "Hide";
+
+        progressBar.interactable = false; 
 
 
         redSlider.maxValue = 1.0f;
@@ -50,11 +61,11 @@ public class UI_Script : MonoBehaviour
         blueSlider.minValue = 0.0f;
         alphaSlider.minValue = 0.0f;
 
-
         redSlider.value = 1.0f;
         greenSlider.value = 1.0f;
         blueSlider.value = 1.0f;
         alphaSlider.value = 1.0f;
+
     }
 
     private void Update()
@@ -62,6 +73,30 @@ public class UI_Script : MonoBehaviour
         Color newColor = new Color(redSlider.value, greenSlider.value, blueSlider.value, alphaSlider.value);
         imageRGB.color = newColor;
     }
+
+
+
+    public void CoinFound()
+    {
+        if (progressBar.value < progressBar.maxValue)
+        {
+            progressBar.value += 1;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public void UpdateScore(int num)
     {
@@ -92,17 +127,19 @@ public class UI_Script : MonoBehaviour
 
 
 
-    public void HideCoin()
+
+
+    public void HidePNG()
     {
-        if (coin.activeInHierarchy)
+        if (imagePNG.activeInHierarchy)
         {
-            coin.SetActive(false);
-            textCoin.text = "Show";
+            imagePNG.SetActive(false);
+            textPNG.text = "Show";
         }
         else
         {
-            coin.SetActive(true);
-            textCoin.text = "Hide";
+            imagePNG.SetActive(true);
+            textPNG.text = "Hide";
         }
         
     }
